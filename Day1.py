4a977eb7ebf3043dfd9e1ee3a5a6ccac023bea5b
anisharai11 <tamlarai.anisha@gmail.com>
2025-07-10 12:29:27 +0545

    table of code

---
+++ Day1.py
@@ -0,0 +1,15 @@
+#modules
+# 2. built in modules
+# 1. external modules
+
+print("hello world")
+
+
+#comments
+'''
+this is multiline comments
+
+'''
+
+#Write a program to print Twinkle twinkle little star poem in python.
+#To print the Twinkle Twinkle Little Star Poem
---
+++ Table5.py
@@ -0,0 +1,64 @@
+#Operators
+
+#arithmetic operators
+# +, -, *, /, %, **
+
+#Comparsion operators
+# ==, !=, >, <, >=, <=, =
+
+# == ===
+ 
+ #logical operators
+ # and, or, not
+
+a = 5
+b = 10
+#a=b
+print(a)
+print(a==b)
+
+a=3
+b=5
+a=b
+print (a)
+print(a==b)
+
+print(10%4)
+
+
+base = 5
+
+for i in range(1, 11):
+    result = base * i
+    print(f"{base} *{i} = {result}")
+
+age = 25
+print("age is age")
+
+
+age = 25
+print(f"Age is {age}")
+
+#Write a program to calculate simple interest
+p = 200
+t = 2
+r = 1
+SI = (p*t*r)/100
+print(SI)
+
+base = int(input("enter any number:"))
+
+for i in range(1, 11):
+    result  = base * i
+    print(f"{base}* {i} = {result}")
+
+
+#Write a program to print the sum of 2 number entered by the user
+sum1 = float(input("enter any numbers : "))
+sum2 = float(input("enter any numbers : "))
+total = sum1 + sum2
+print("total = " , total)
+
+#Write a python program to find reminder when a number is divided by 2
+#Write a python program to display a user entered name followed by Good Afternoon using input() function.
+#Write a program to fill in a letter template given below with the name and date
